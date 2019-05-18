e03189fe45100559a6241f55b782b465224f9b25
Sayne Rojas <saynegrojas@yahoo.com>
2019-05-17 20:06:40 -0700

    Crystal collector

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # unit-4-game
 crystal collector
+
+Here's how the app works:
+
+There will be four crystals displayed as buttons on the page.
+
+The player will be shown a random number at the start of the game.
+
+When the player clicks on a crystal, a random number will be added to the player's total score.
+
+The player wins if the total score matches the random number displayed from the beginning of the game.
+
+The player loses if their score goes above the random number.
+
+The game restarts whenever the player wins or loses.
+
+When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. And the user's score will reset to zero.
+
+There's a tracker of the number of games the player wins and loses.
+
+
+
+
---
+++ assets/javascript/game.js
@@ -0,0 +1,87 @@
+$(document ).ready(function() {
+    //object for all the crystals
+        var crystals = {
+            green: {image: "green", value: 0},
+            red: {image: "red", value: 0},
+            silver: {image: "silver", value: 0},
+            yellow: {image: "yellow", value: 0},
+        };
+    
+    //variables for wins,losses,counter, target
+        var targetNum = 0,
+            counter = 0,
+            wins = 0,
+            losses = 0;
+    
+        function start() {
+            //counter gets set to 0 whenever called function is called
+            counter = 0;
+            //random number on the target box
+            targetNum = (Math.floor(Math.random() * 102) + 19);
+            $(".randomNumberBox").html(targetNum);
+    
+            //random number everytime a crystal is clicked
+            $("#green").on("click", function () {
+                crystals.green.value = Math.floor(Math.random() * 12) + 1;
+            });
+            $("#red").on("click", function (){
+                crystals.red.value = Math.floor(Math.random() * 12) + 1;
+            });
+            $("#silver").on("click", function (){
+                crystals.silver.value = Math.floor(Math.random() * 12) + 1;
+            });
+            $("#yellow").on("click", function (){
+                crystals.yellow.value = Math.floor(Math.random() * 12) + 1;
+            });
+        };
+        //adds crystals to total score
+        function adding(crystals) {
+            counter += crystals.value;
+            $(".totalScoreBox").html(counter);
+            check();
+        }
+        //checks if counter matches or goes over 
+        //?? does if/else if need else ?
+        function check() {
+            if (counter > targetNum) {
+                losses++;
+                lossesText();
+                $("#loss").html(losses);
+                start();
+            } else if (counter === targetNum) {
+                wins++;
+                winsText();
+                $("#wins").html(wins);
+                start();
+            }
+        }
+        //start function called 
+        start();
+    
+        //adds and displays inside the total score box
+        $("#green").on("click", function () {
+            adding(crystals.green);
+        });
+        $("#red").on("click", function () {
+            adding(crystals.red);
+        });
+        $("#silver").on("click", function () {
+            adding(crystals.silver);
+        });
+        $("#yellow").on("click", function () {
+            adding(crystals.yellow);
+        });
+        
+        //function alerts when results occur
+        function winsText(){
+            setTimeout(function () {
+                alert("CONGRATS, YOU WON!");
+            }, 500);
+        };
+        function lossesText(){
+            setTimeout(function () {
+                alert("SORRY, YOU LOST!");
+            }, 500);
+        }
+    });
+    
